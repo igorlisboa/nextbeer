aa8ef0d74a377c251d278fa6f439197b66435e92
1nickael1 <qaznickael@hotmail.com>
2018-11-24 21:14:46 -0200

    Criação da página Criar comandas

--- web/adm-servico/src/funcionalidades/cardapio/Cardapio.js
+++ web/adm-servico/src/funcionalidades/cardapio/Cardapio.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Card, CardBody, CardHeader, Col, Row} from 'reactstrap';
+import {Button, Card, CardBody, CardHeader, Col, Row, ListGroup, ListGroupItem, Badge} from 'reactstrap';
 
 import {CardapioSub} from './CardapioSub';
 import IncluirEditarCardapio from './IncluirEditarCardapio';
@@ -25,6 +25,73 @@ export default class Cardapio extends Component{
   render(){
     return (
       <div>
+        <div className="animated fadeIn">
+          <Row>
+            <Col>
+              <Card>
+                <CardHeader className="pb-2">
+                  <i className="icons icon-book-open font-3xl align-middle"></i> <span className="font-weight-bold font-4xl text-uppercase ml-3 align-middle">Comanda mesa 1</span>
+                </CardHeader>
+                <CardBody>
+                  <Row>
+                    <Col md="6" className="card">
+                      <div class="mt-1">
+                        <Row>
+                          <Col md="1">
+                            <i className="icons icon-note font-2xl align-middle"></i>
+                          </Col>
+                          <Col md="5">
+                            <div className="text-value">{"joao"}</div>
+                          </Col>
+                          <Col md="3">
+                            <Button color="warning" className="w-100 float-right align-middle btn-normal text"><span>Alterar</span></Button>
+                          </Col>
+                          <Col md="3">
+                            <Button color="danger" className="w-100 float-right align-middle btn-normal text"><span>Excluir</span></Button>
+                          </Col>
+                        </Row>
+                      </div>
+                      <div class="mt-1">
+                        <Row>
+                          <Col md="1">
+                            <i className="icons icon-note font-2xl align-middle"></i>
+                          </Col>
+                          <Col md="5">
+                            <div className="text-value">{"Monica"}</div>
+                          </Col>
+                          <Col md="3">
+                            <Button color="warning" className="w-100 float-right align-middle btn-normal text"><span>Alterar</span></Button>
+                          </Col>
+                          <Col md="3">
+                            <Button color="danger" className="w-100 float-right align-middle btn-normal text"><span>Excluir</span></Button>
+                          </Col>
+                        </Row>
+                      </div>
+                    </Col>
+
+                    <Col md="6" className="card">
+                      <div class="mt-1">
+                        <CardHeader>
+                        <div className={""}>
+                          <span className="w-100 font-weight-bold text-uppercase align-top">Comanda Joao</span>
+                        </div>
+                        </CardHeader>
+                        <ListGroup>
+                          <ListGroupItem className="justify-content-between">Chopp <Badge pill>x6</Badge></ListGroupItem>
+                          <ListGroupItem className="justify-content-between">Batata frita <Badge pill>x2</Badge></ListGroupItem>
+                          <ListGroupItem className="justify-content-between">Hamburguer <Badge pill>x1</Badge></ListGroupItem>
+                        </ListGroup>
+                        <div>
+                          <Button color="danger" className="w-100 float-right align-middle btn-normal text"><span>Criar comanda</span></Button>
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
         <div className="animated fadeIn">
           <Row>
             <Col>
