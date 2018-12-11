1ff8dd2755c6e8a3852d973bd1c0a3ca48cfdce1
1nickael1 <qaznickael@hotmail.com>
2018-12-11 01:39:19 -0200

    front cliente

--- web/app-cliente/src/components/home.js
+++ web/app-cliente/src/components/home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import GuestLayout from './guest-layout';
-import {Card, CardHeader, CardBody, Row, Col} from "reactstrap";
+import { Card, CardHeader, CardBody, Row, Col, Table, Button,
+Form, FormGroup, Label, Input } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Column = (props) => {
   const { title, description, link } = props;
@@ -26,63 +28,74 @@ Column.propTypes = {
 
 export default () => (
   <GuestLayout>
-      <div className="columns is-centered p-t-md p-r-md p-l-md">
-        <div className="box">
-          <h1 className="title">Comanda</h1>
-          <div className="box">
-            <div className="table">
-              <table className="md-12">
-                <thead>
-                  <tr>
-                    <th className="col-md-1">Pedidos</th>
-                    <th>Quantidade</th>
-                    <th>Preço</th>
-                    <th>Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>Hamburguer</th>
-                    <th>3</th>
-                    <th>R$5,00</th>
-                    <th>R$15,00</th>
-                  </tr>
-                  <tr>
-                    <th>Colca Cola</th>
-                    <th>2</th>
-                    <th>R$3,50</th>
-                    <th>R$7,00</th>
-                  </tr>
-                  <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th>R$22,00</th>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-
-        </div>
-
-
-      </div>
-    <div>
-      <Card >
-        <CardHeader>
-          <span>Teste</span>
-        </CardHeader>
+    <div className="container">
+    <Card className="mb-3">
+        <CardHeader>Login</CardHeader>
         <CardBody>
-          <Row>
-            <Col>
-              <Card>
-                <h1>Ola</h1>
-              </Card>
-            </Col>
-          </Row>
+          <Form inline>
+          
+            <FormGroup>
+              <Label for="exampleEmail" className="mr-3">Mesa</Label>
+              <Input type="password" className="mr-3" name="mesa" id="exampleEmail" placeholder="Número da mesa" />
+            </FormGroup>
+            
+            <FormGroup>
+              <Label for="examplePassword" className="mr-3">Senha</Label>
+              <Input type="password" className="mr-3" name="senha" id="examplePassword" placeholder="Senha da mesa" />
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
         </CardBody>
       </Card>
+
+      <Card className="w-100 mb-3">
+        <CardHeader>Comanda</CardHeader>
+        <CardBody className="">
+          <Table bordered className="col-12">
+            <thead>
+              <tr>
+                <th>Pedidos</th>
+                <th>Quantidade</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Hamburguer</th>
+                <th className="text-center">3</th>
+                <th>R$15,00</th>
+              </tr>
+              <tr>
+                <th>Colca Cola</th>
+                <th className="text-center">2</th>
+                <th>R$7,00</th>
+              </tr>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>R$22,00</th>
+              </tr>
+            </tbody>
+
+          </Table>
+        </CardBody>
+      </Card>
+
+      <Card className="mb-3">
+        <CardHeader>Cardapio
+          </CardHeader>
+        <CardBody>
+          Cardapio 1
+          Cardapio 2
+          Cardapio 3
+          Cardapio 4
+            </CardBody>
+      </Card>
+
+      
+
+
+
     </div>
 
   </GuestLayout>
